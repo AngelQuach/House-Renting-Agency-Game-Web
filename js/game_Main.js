9639c6aeb1478054b5d67ace92bf883a9d808a34
Angel Quach <122626373+AngelQuach@users.noreply.github.com>
2024-03-26 00:09:41 -0400

    Update game_Main.js

--- js/game_Main.js
+++ js/game_Main.js
@@ -7,14 +7,14 @@ var clickSound = new Audio('clickSound.m4a');
 var bgSound = new Audio('bgMusic.m4a');
 
 //preload the sounds
-clickSound.preload = "auto";
-bgSound.preload = "auto";
+//clickSound.preload = "auto";
+//bgSound.preload = "auto";
 
 //add actionListeners to the buttons
 startButton.addEventListener('click', function(){
 	console.log("Start button clicked"); //log a message for checking purpose
 	clickSound.play();
-	//bgSound.play();
+	bgSound.play();
 });
 
 moreInfoButton.addEventListener('click', function(){
