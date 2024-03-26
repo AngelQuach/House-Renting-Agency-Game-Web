368f1772ddadb60016ca0783490fe9af4f691659
Angel Quach <122626373+AngelQuach@users.noreply.github.com>
2024-03-26 00:59:52 -0400

    Update game_Main.js

--- js/game_Main.js
+++ js/game_Main.js
@@ -4,15 +4,18 @@ var moreInfoButton = document.getElementById("MIButton");
 
 //introduce sound effects
 var clickSound = new Audio('clickSound.m4a');
-var bgSound = new Audio('bgMusic.mp3');
+var bgSound = new Audio('bgMusic.m4a');
 
 //add actionListeners to the buttons
 startButton.addEventListener('click', function(){
-	console.log("Start button clicked"); //log a message for checking purpose
 	clickSound.play();
 	bgSound.play();
+	//redirect to the game page
+	//window.location.href = 'game.html';
 });
 
 moreInfoButton.addEventListener('click', function(){
 	clickSound.play();
+	//shows the author information
+	window.location.href = 'authorInfo.html';
 });
