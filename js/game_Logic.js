9eb9d9c9d04cb6975b691e45ee75d46c869e08f3
Angel Quach <122626373+AngelQuach@users.noreply.github.com>
2024-03-27 12:51:14 -0400

    Create game_Logic.js

---
+++ js/game_Logic.js
@@ -0,0 +1,34 @@
+//set up the dialog format
+var gamedialog = {
+    drawBounds: function(x, y, width, height, radius){
+        var ctx = document.context;
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 4;
+
+        //reset the path
+        ctx.beginPath();
+        ctx.moveTo(x + radius, y);
+        //draw top length
+        ctx.lineTo(x + radius + width, y);
+        //draw right top corner
+        ctx.arcTo(x + radius + width, y, x + 2*radius + width, y + radius, radius);
+        //draw right length
+        ctx.lineTo(x + 2*radius + width, y + radius + height);
+        //draw right bottom corner
+        ctx.arcTo(x + 2*radius + width, y + radius + height, x + radius + width, y + 2*radius + height, radius);
+        //draw bottom length
+        ctx.lineTo(x + radius, y + 2*radius + height);
+        //draw left bottom corner
+        ctx.arcTo(x + radius, y + 2*radius + height, x, y + radius + height, radius);
+        //draw left length
+        ctx.lineTo(x, y + radius);
+        //draw left top corner
+        ctx.arcTo(x, y + radius, x + radius, y, radius);
+        ctx.closePath();
+
+        //fill the window with color
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        ctx.fill();
+    }
+}
+gamedialog.drawBounds(100, 400, 350, 250, 10);
